983573387f5126e242744102789066c4fcde18ac
javier.sanchez <kornjaviersanchezb@gmail.com>
2024-08-19 22:39:04 -0500

    update version node 20

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-app.get("/", (req, res) => {
+// Route handler for the root URL
+app.get('/', (req, res) => {
   const htmlResponse = `
     <html>
       <head>
         <title>NodeJs y Express en Vercel</title>
       </head>
       <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
+        <h1>Soy un proyecto Back end en Vercel</h1>
       </body>
     </html>
   `;
   res.send(htmlResponse);
 });
 
+// Export the app as a handler function
 module.exports = (req, res) => {
+  // Pass the request and response objects to the Express app
   app(req, res);
 };
