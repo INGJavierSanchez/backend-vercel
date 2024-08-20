9f88e9e10cb7c44f316d5e9a0d547079745fbeaf
javier.sanchez <kornjaviersanchezb@gmail.com>
2024-08-19 22:50:21 -0500

    update index.js del api

--- api/index.js
+++ api/index.js
@@ -7,30 +7,3 @@ app.use(bodyParser.json());
 app.use('/clientes', clientesRouter);
 
 module.exports = app;
-
-const express = require('express');
-
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Route handler for the root URL
-app.get('/', (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en Vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
-});
-
-// Export the app as a handler function
-module.exports = (req, res) => {
-  // Pass the request and response objects to the Express app
-  app(req, res);
-};
